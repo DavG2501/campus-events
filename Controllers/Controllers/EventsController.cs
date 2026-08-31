65d25b88c30fdd766dab77b2196ae5b295c29611
DavG2501 <dgarces0262@interbayamon.edu>
2026-08-31 18:29:41 -0400

    Create CEMS MVC shell and Events page

---
+++ Controllers/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CampusEvents.Controllers;
+
+public class EventsController : Controller
+{
+	private readonly ILogger<EventsController> _logger;
+
+	public EventsController(ILogger<EventsController> logger)
+	{
+		_logger = logger;
+	}
+
+	public IActionResult Index()
+	{
+		_logger.LogInformation("Handling request for the CEMS Events page.");
+
+		var featuredEventTitle = "Campus Welcome Mixer2";
+
+		return View(model: featuredEventTitle);
+	}
+
+}
